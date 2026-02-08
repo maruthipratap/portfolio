4bd9d7a58aef204ff2614356b942291e5f45c6c4
maruthipratap <maruthiprathap66@gmail.com>
2026-02-09 01:56:52 +0530

    Improve UI design and add active navbar highlight

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded",() => {
-    console.log("Portfolio Loaded Successfully");
-})
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 150;
+        const sectionHeight = section.clientHeight;
+
+        if (pageYOffset >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
