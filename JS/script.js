443a147491f927125b3c72874108310976869c88
maruthipratap <maruthiprathap66@gmail.com>
2026-02-09 23:03:12 +0530

    Play project demo video in new tab player

--- JS/script.js
+++ JS/script.js
@@ -35,3 +35,46 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach(el => observer.observe(el));
 
+/* video view */
+function openDemo() {
+    const videoWindow = window.open("", "_blank");
+
+    videoWindow.document.write(`
+        <html>
+        <head>
+            <title>Project Demo</title>
+            <style>
+                body{
+                    margin:0;
+                    background:#020617;
+                    display:flex;
+                    justify-content:center;
+                    align-items:center;
+                    height:100vh;
+                    flex-direction:column;
+                    color:white;
+                    font-family:Arial;
+                }
+                h2{
+                    color:#22d3ee;
+                    margin-bottom:20px;
+                }
+                video{
+                    width:90%;
+                    max-width:900px;
+                    border-radius:12px;
+                    box-shadow:0 0 30px rgba(0,0,0,0.6);
+                }
+            </style>
+        </head>
+
+        <body>
+            <h2>Smart LPG Gas Detection System — Demo</h2>
+            <video controls autoplay>
+                <source src="images/lpg_demo.mp4" type="video/mp4">
+                Your browser does not support video.
+            </video>
+        </body>
+        </html>
+    `);
+}
